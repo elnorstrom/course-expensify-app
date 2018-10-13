6a3bf3a98725b9735abf379c0961ed5aeb072b90
Home <elias.norstromm@gmail.com>
2018-10-13 09:21:19 +0100

    Initial commit

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/Header';
+
+test('should render header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+
+// import toJSON from 'enzyme-to-json';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
